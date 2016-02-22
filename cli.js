32a6ca238de2390330e2c08b7aa90c8f7f8c12fb
Geir Gåsodden <geir.gasodden@pythonia.no>
2016-02-22 09:09:03 +0100

    Separates API and CLI

---
+++ cli.js
@@ -0,0 +1,34 @@
+#! /usr/bin/env node
+'use strict'
+
+require.extensions['.sql'] = function (module, filename) {
+  module.exports = fs.readFileSync(filename, 'utf8')
+}
+
+// External requirements
+var fs = require('fs')
+var opn = require('opn')
+
+// Local requirements
+var getListFromDb = require('./index')
+var query = process.argv[2]
+var filename = query + '.txt'
+
+getListFromDb(query, function (error, data) {
+  if (error) {
+    console.error(error)
+    process.exit(1)
+  }
+
+  var list = []
+
+  data.forEach(function (item) {
+    list.push(item.id)
+  })
+
+  fs.writeFileSync(filename, list.join('\n'))
+
+  opn(filename)
+
+  process.exit(0)
+})
--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-#! /usr/bin/env node
 'use strict'
 
 require.extensions['.sql'] = function (module, filename) {
@@ -7,50 +6,38 @@ require.extensions['.sql'] = function (module, filename) {
 
 // External requirements
 var fs = require('fs')
-var opn = require('opn')
 
 // Local requirements
 var runQuery = require('./lib/runQuery')
 
 var qryFindDuplicateContacts = require('./lib/sql/findDuplicateContacts.sql')
 var qryGetStudentFolders = require('./lib/sql/getStudentFolders.sql')
-
-var filename = process.argv[2] + '.txt'
-
+var validTypes = ['elevmappe', 'kontakt']
 var query
 
-function getListFromDb (callback) {
-  runQuery(query, function (err, data) {
-    if (err) {
-      return callback(err)
+function getListFromDb (type, callback) {
+  if (!type) {
+    return callback(new Error('Missing required input: type'), null)
+  }
+  if (validTypes.indexOf(type) === -1) {
+    return callback(new Error('Invalid type. Valid types are ' + validTypes.join(', ')), null)
+  }
+
+  switch (type) {
+    case 'elevmappe':
+      query = qryGetStudentFolders
+      break
+    case 'kontakt':
+      query = qryFindDuplicateContacts
+      break
+  }
+
+  runQuery(query, function (error, data) {
+    if (error) {
+      return callback(error)
     }
     return callback(null, data)
   })
 }
 
-switch (process.argv[2]) {
-  case 'elevmappe':
-    query = qryGetStudentFolders
-    break
-  case 'kontakt':
-    query = qryFindDuplicateContacts
-    break
-  default:
-    console.log('Bruk med parameter kontakt eller elevmappe')
-    process.exit(1)
-}
-
-getListFromDb(function (err, data) {
-  if (err) {
-    console.log(err)
-    process.exit(1)
-  }
-  var list = []
-
-  data.forEach(function (item) {
-    list.push(item.id)
-  })
-  fs.writeFileSync(filename, list.join('\n'))
-  opn(filename)
-  process.exit(0)
-})
+module.exports = getListFromDb
